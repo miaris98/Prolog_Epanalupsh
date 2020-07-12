5334d5a3519b6e154ccf233533ccef4d4f054a32
dennis <39520771+miaris98@users.noreply.github.com>
2020-07-12 18:35:20 +0300

    Add files via upload

---
+++ Ask8_Ask10.pl
@@ -0,0 +1,31 @@
+%ask10
+katathetis(N , [ [N,P]|T] , P).
+katathetis(N , [ [N1,P1]|T ] , P) :-
+katathetis(N,T,P).
+
+
+
+metafora([[N,P]|T],P).	
+
+metafora([[N,P]|T],LH):-
+	metafora(T,LN),
+	LH is P + LN.
+%ask8
+
+
+encrypted_parts(Encrypted, Prefix, X, Y, Suffix, Final) :-
+    append(Prefix, L1, Encrypted),
+    L1 = [X, X | L2],
+    append(Final, L3, L2),
+    L3 = [Y, Y | L4],
+    Suffix = L4.
+	
+decrypted_parts(Decrypted, Prefix, X, Y, Suffix, Final) :-
+    append(Prefix, L1, Decrypted),
+    L1 = [X, Y | L2],
+    append(Suffix, L3, L2),
+    Final = L3.	
+
+cryp(Encrypted, Decrypted) :-
+    encrypted_parts(Encrypted, Prefix, X, Y, Suffix, Final),
+    decrypted_parts(Decrypted, Prefix, X, Y, Suffix, Final).
